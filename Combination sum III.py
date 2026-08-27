a6c861e387425ecd33e82f2b9edd1dd0551f85e7
Ramyamv24 <ramyavani689@gmail.com>
2026-08-27 17:15:14 +0530

    combination sum III

---
+++ Combination sum III.py
@@ -0,0 +1,31 @@
+from typing import List
+
+class Solution:
+    def combinationSum3(self, k: int, n: int) -> List[List[int]]:
+        res = []
+
+        def backtrack(start, path, target, k):
+            if target == 0 and k == 0:
+                res.append(path[:])
+                return
+
+            for i in range(start, 10):
+                if i > target or k <= 0:
+                    break
+
+                path.append(i)
+                backtrack(i + 1, path, target - i, k - 1)
+                path.pop()
+
+        backtrack(1, [], n, k)
+        return res
+
+
+# Main part
+k = int(input())
+n = int(input())
+
+obj = Solution()
+result = obj.combinationSum3(k, n)
+
+print(result)
